c3353ea35a8a79acfddf3090a3958e97dff161f7
Nayab Naeem <nayabnaeem666@gmail.com>
2026-02-20 21:13:23 +0500

    Special Binary String problem solution

---
+++ MathLogic/SpecialBinaryString.cpp
@@ -0,0 +1,15 @@
+Given a special binary string s, you can swap two consecutive 
+non-empty special substrings any number of times.
+
+Return the lexicographically largest string possible.
+*/
+
+/*
+Problem       : 761. Special Binary String
+Platform      : LeetCode
+Difficulty    : Hard
+Topic         : Recursion, Greedy, String
+Language      : C++
+Time Complexity  : O(n^2 log n)
+Space Complexity : O(n)
+*/
