eb689f6c17d81a666c1e49f0a3f3879f311150ff
Nayab Naeem <nayabnaeem666@gmail.com>
2026-03-08 00:22:54 +0500

    Binary string solution update

--- Arrays+Strings/BinaryStrinsSegment.cpp
+++ Arrays+Strings/BinaryStrinsSegment.cpp
@@ -19,50 +19,15 @@ Space Complexity: O(1)
 ---------------------------------------------------------
 */
 
-/*
----------------------------------------------------------
-Approach: Detect Second Segment of Ones
----------------------------------------------------------
-
-Observation:
-If a '1' appears again after a '0', then there are two
-segments of ones.
-
-Example:
-s = "1001"
-
-1st segment → "1"
-Then '0' appears
-Then another '1' appears → second segment → invalid
-
-Algorithm:
-1. Traverse the string from index 0 to n-2.
-2. Check if pattern "01" appears.
-3. If "01" exists → return false.
-4. If traversal completes → return true.
-
-Why It Works:
-"01" means zeros ended and ones started again,
-which indicates a second segment of ones.
-
-Time Complexity:
-O(n) → single traversal of string
-
-Space Complexity:
-O(1) → no extra space used
----------------------------------------------------------
-*/
 
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-
         for(int i = 0; i < s.length() - 1; i++)
         {
-            if(s[i] == '0' && s[i+1] == '1')
+            if(s[i] == '0' && s[i+1] == '1')  //comparison of current and next character
                 return false;
         }
-
         return true;
     }
 };
